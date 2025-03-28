cca3dd7e074909839bd091a771e18e5c261ea8be
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-28 13:08:57 -0700

    wip: update source code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 import PokemonList from './components/PokemonList/PokemonList';
+import PokemonDetails from './components/PokemonDetails/PokemonDetails';
+import PokemonForm from './components/PokemonForm/PokemonForm';
+import NavBar from './components/NavBar/NavBar';
+import { Routes, Route } from 'react-router';
+
 
 const initialState = [
   { _id: 1, name: 'bulbasaur', weight: 69, height: 7 },
@@ -11,15 +16,37 @@ const initialState = [
 
 
 
-
 function App() {
-  const [pokemon, setPokemon] = useState(initialState)
+  const [pokemon, setPokemon] = useState(initialState);
 
+  const  addPokemon = (newPokemonData) => {
+    newPokemonData._id = pokemon.length + 1;
+    setPokemon([...pokemon, newPokemonData]);
+  }
   return (
     <>
       <h1>Pokemon!</h1>
-      <PokemonList pokemon={pokemon}/>
-
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<h2>Welcome to Pokemon App!</h2>} />
+        <Route 
+          path='/pokemon' 
+          element={<PokemonList pokemon={pokemon} />}
+        />
+        <Route
+          path='/pokemon/new'
+          element={<PokemonForm addPokemon={addPokemon}/>}
+        />
+        {/* updated route passing PokemonForm the addPokemon Prop */}
+        <Route
+          path='/pokemon/:pokemonId'
+          element={<PokemonDetails pokemon={pokemon} />}
+        />
+        <Route
+          path='*'
+          element={<h2>Whoops, nothing here!</h2>}
+        />
+       </Routes>
     </>
   )
 }
